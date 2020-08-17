216e9d53a6ca922eaf8758fd7bda446ddb97f006
Tim Wong <wongkk25@users.noreply.github.com>
2020-08-17 02:06:54 -0400

    added some dev tools to setup script

--- setup-ubuntu.sh
+++ setup-ubuntu.sh
@@ -9,7 +9,12 @@ sudo apt-get install \
 	adb \
   flatpak \
 	gparted \
-	virtualbox
+	htop \
+	nodejs \
+	virtualbox \
+	zsh
+
+chsh -s /bin/zsh
 
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 
@@ -36,5 +41,16 @@ sudo apt-get upgrade
 sudo add-apt-repository ppa:graphics-drivers/ppa
 sudo apt-get update
 sudo apt-get install nvidia-graphics-drivers-450
+
+# pyenv
+curl https://pyenv.run | bash
+reset
+pyenv install 3.8
+
+#rvm
+gpg2 --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
+curl -sSL https://get.rvm.io | bash -s stable
+reset
+rvm install ruby --latest
 
 sudo reboot
