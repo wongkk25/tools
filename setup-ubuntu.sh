87e47f8a814458b936bd2a77290ecb95a4b40178
Tim Wong <wongkk25@users.noreply.github.com>
2020-08-17 05:57:19 -0400

    swapped flatpak vscode for snap package, added some more apt packages and todos

--- setup-ubuntu.sh
+++ setup-ubuntu.sh
@@ -7,14 +7,19 @@ sudo add-apt-repository ppa:alexlarsson/flatpak
 sudo apt-get update
 sudo apt-get install \
 	adb \
-  flatpak \
+  	flatpak \
+  	git \
 	gparted \
+	gnupg2 \
 	htop \
 	nodejs \
+	tmux \
 	virtualbox \
 	zsh
 
 chsh -s /bin/zsh
+
+sudo snap install code
 
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 
@@ -23,7 +28,6 @@ flatpak install flathub \
 	com.jetbrains.IntelliJ-IDEA-Community \
 	com.slack.Slack \
 	com.spotify.Client \
-	com.visualstudio.code \
  	org.audacityteam.Audacity \
 	org.darktable.Darktable \
 	org.gimp.GIMP \
@@ -37,20 +41,22 @@ curl https://rclone.org/install.sh | sudo bash
 # lastly, upgrade existing binaries
 sudo apt-get upgrade
 
+# todo nvidia-graphics-drivers is broken
 # nvidia drivers
 sudo add-apt-repository ppa:graphics-drivers/ppa
 sudo apt-get update
 sudo apt-get install nvidia-graphics-drivers-450
 
+# todo move to separate setup file, fix broken installation of this + needs dependencies
 # pyenv
 curl https://pyenv.run | bash
-reset
+source ./
 pyenv install 3.8
 
-#rvm
+# todo move to separate setup file
+# rvm
 gpg2 --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
 curl -sSL https://get.rvm.io | bash -s stable
-reset
 rvm install ruby --latest
 
-sudo reboot
+echo "Installation complete, restart computer"
