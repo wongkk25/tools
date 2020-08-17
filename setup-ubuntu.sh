9e4fdf59837d71904d0c219ae21a4b1369a48a23
Tim Wong <wongkk25@users.noreply.github.com>
2020-08-17 02:11:56 -0400

    added hosts file edit

--- setup-ubuntu.sh
+++ setup-ubuntu.sh
@@ -48,10 +48,13 @@ reset
 pyenv latest install
 pyenv latest install 2.7
 
-#rvm
+# rvm
 gpg2 --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
 curl -sSL https://get.rvm.io | bash -s stable
 reset
 rvm install ruby --latest
+
+# hosts
+sudo zsh -c ' wget https://someonewhocares.org/hosts/zero/hosts > /etc/hosts'
 
 sudo reboot
