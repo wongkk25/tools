33c39c794c6b7d6b911ac77c5233686f4b69b5cf
Tim Wong <twong@pop-os.localdomain>
2020-08-26 11:03:41 -0400

    updated ubuntu scripts with randomness

--- setup-ubuntu/setup-ubuntu.sh
+++ setup-ubuntu/setup-ubuntu.sh
@@ -3,16 +3,19 @@
 # script to install a bunch of random apps I use sometimes, after OS clean install
 # for Ubuntu based distros
 
-chmod 755 *
+#sudo add-apt-repository ppa:alexlarsson/flatpak
 
-sudo add-apt-repository ppa:alexlarsson/flatpak
+# vscodium
+wget -qO - https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/raw/master/pub.gpg | sudo apt-key add -
+echo 'deb https://paulcarroty.gitlab.io/vscodium-deb-rpm-repo/debs/ vscodium main' | sudo tee --append /etc/apt/sources.list.d/vscodium.list
+
 sudo apt-get update
 sudo apt-get install \
 	adb \
+	codium \
 	docker.io \
 	fcitx-bin \
 	fcitx-googlepinyin \
-	fcitx-frontend-qt5 \
 	fcitx-mozc \
   	flatpak \
   	git \
@@ -26,26 +29,18 @@ sudo apt-get install \
 
 sudo ./setup-darktable-ubuntu.sh
 
-chsh -s /bin/zsh
+#flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 
-# zsh has some issues with detecting snap packages; below line should fix them
-emulate sh -c 'source /etc/profile.d/apps-bin-path.sh'
-
-sudo snap install code
-
-flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
-
-flatpak install flathub \
-	com.discordapp.Discord \
-	com.jetbrains.IntelliJ-IDEA-Community \
-	com.slack.Slack \
-	com.spotify.Client \
- 	org.audacityteam.Audacity \
-	org.gimp.GIMP \
-	org.libreoffice.LibreOffice \
-	org.mozilla.Thunderbird \
-	org.octave.Octave \
-	org.signal.Signal
+#flatpak install flathub \
+#	com.discordapp.Discord \
+#	com.slack.Slack \
+#	com.spotify.Client \
+ #	org.audacityteam.Audacity \
+#	org.gimp.GIMP \
+#	org.libreoffice.LibreOffice \
+#	org.mozilla.Thunderbird \
+#	org.octave.Octave \
+#	org.signal.Signal
 
 curl https://rclone.org/install.sh | sudo bash
 
@@ -54,5 +49,7 @@ sudo apt-get upgrade
 
 sudo ./setup-pyenv-ubuntu.sh
 sudo ./setup-rvm.sh
+
+chsh -s /bin/zsh
 
 echo "Installation complete, restart computer"
