7076f2a63d9337f30db81e8c19c7e80f6a2c5452
Tim Wong <wongkk25@users.noreply.github.com>
2020-09-08 18:14:03 -0400

    swapped shell

--- setup-ubuntu/setup-pyenv-ubuntu.sh
+++ setup-ubuntu/setup-pyenv-ubuntu.sh
@@ -6,7 +6,7 @@ sudo apt-get install -y build-essential libssl-dev zlib1g-dev libbz2-dev \
 	libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev libncursesw5-dev \
 	xz-utils tk-dev libffi-dev liblzma-dev python-openssl git
 
-exec zsh
+exec bash
 
 pyenv install 3.8.15
 pyenv install 2.7.18
