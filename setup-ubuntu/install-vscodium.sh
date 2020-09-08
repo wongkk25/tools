95cd5bdf5e4357888ee9edc54dbccdffb2fca5d5
Tim Wong <wongkk25@users.noreply.github.com>
2020-09-08 18:58:59 -0400

    added install script for vscodium

---
+++ setup-ubuntu/install-vscodium.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+wget -qO - https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/raw/master/pub.gpg | sudo apt-key add -
+echo 'deb https://paulcarroty.gitlab.io/vscodium-deb-rpm-repo/debs/ vscodium main' | sudo tee --append /etc/apt/sources.list.d/vscodium.list
+sudo apt update && sudo apt upgrade
