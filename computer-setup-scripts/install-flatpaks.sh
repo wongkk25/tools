8b519c4e06f488dc55c187f8473ea920c45d86bb
Tim Wong <wongkk25@users.noreply.github.com>
2020-09-22 18:33:35 -0400

    removed intellij flatpak

--- computer-setup-scripts/install-flatpaks.sh
+++ computer-setup-scripts/install-flatpaks.sh
@@ -4,6 +4,5 @@ flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flat
 
 flatpak install flathub \
 	com.discordapp.Discord \
-	com.jetbrains.IntelliJ-IDEA-Community \
 	com.spotify.Client \
 	com.slack.Slack 
