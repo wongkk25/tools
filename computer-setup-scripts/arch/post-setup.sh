88dbfaa433a47744909e3c2cc3ff46f838184403
Tim Wong <wongkk25@users.noreply.github.com>
2020-09-22 00:51:01 -0400

    added initial arch setup script

---
+++ computer-setup-scripts/arch/post-setup.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+# Arch post setup script
+
+pacman -S adb \
+	audacity \
+	compton \
+	darktable \
+	docker \
+	fcitx \
+	fcitx-googlepinyin \
+	fcitx-mozc \
+	firefox \
+	feh \
+	git \
+	gimp \
+	gparted \
+	gnupg2 \
+	htop \
+	i3-gaps \
+	octave \
+	rclone \
+	neovim \
+	xorg-xinit \
+	xterm \
+	zsh \
+	zsh-completions
