81d31c2a48703144bcf55206eaea0db70a1e08d8
Tim Wong <wongkk25@users.noreply.github.com>
2021-03-08 18:46:31 -0500

    package updates and moving some apt packages to flatpak

--- computer-setup-scripts/flatpak/install-flatpaks.sh
+++ computer-setup-scripts/flatpak/install-flatpaks.sh
@@ -4,4 +4,5 @@ flatpak install -y flathub \
 	com.discordapp.Discord \
 	com.spotify.Client \
 	com.slack.Slack \
+	org.darktable.Darktable \
 	org.signal.Signal
--- computer-setup-scripts/ubuntu/ubuntu-setup.sh
+++ computer-setup-scripts/ubuntu/ubuntu-setup.sh
@@ -7,14 +7,6 @@
 sudo add-apt-repository -y ppa:ubuntuhandbook1/audacity
 sudo add-apt-repository -y ppa:alexlarsson/flatpak
 
-# darktable
-echo 'deb http://download.opensuse.org/repositories/graphics:/darktable/xUbuntu_20.04/ /' | sudo tee /etc/apt/sources.list.d/graphics:darktable.list
-curl -fsSL https://download.opensuse.org/repositories/graphics:darktable/xUbuntu_20.04/Release.key | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/graphics:darktable.gpg > /dev/null
-
-# signal
-curl -s https://updates.signal.org/desktop/apt/keys.asc | sudo apt-key add -
-echo "deb [arch=amd64] https://updates.signal.org/desktop/apt xenial main" | sudo tee -a /etc/apt/sources.list.d/signal-xenial.list
-
 # vscodium
 wget -qO - https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/raw/master/pub.gpg | sudo apt-key add -
 echo 'deb https://paulcarroty.gitlab.io/vscodium-deb-rpm-repo/debs/ vscodium main' | sudo tee --append /etc/apt/sources.list.d/vscodium.list
@@ -25,8 +17,6 @@ sudo apt-get install -y \
 	audacity \
 	codium \
 	compton \
-	darktable \
-	docker.io \
 	fcitx-bin \
 	fcitx-googlepinyin \
 	fcitx-mozc \
@@ -37,14 +27,13 @@ sudo apt-get install -y \
 	gparted \
 	gnupg2 \
 	htop \
-	i3-gaps \
+	i3-wm \
+	neovim \
 	nodejs \
 	octave \
 	playerctl \
 	rclone \
-	signal-desktop \
 	tmux \
-	vim-gtk \
 	virtualbox \
 	zsh
 
